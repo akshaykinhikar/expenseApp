ba2aa3ef263d229759542bdf1464bf66a3d64bbe
akshaykinhikar <kinhikar.akshay32@gmail.com>
2024-02-07 21:00:13 +0530

    added navbar routing

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -1,15 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddGroup from "./components/AddGroup";
 import AddExpenseComponent from "./components/AddExpenseComponent";
 import NavbarComponent from "./components/NavbarComponent";
 
 function App() {
   return (
-    <div>
-      <NavbarComponent />
-      {/* <AddExpenseComponent></AddExpenseComponent> */}
-      <AddGroup></AddGroup>
 
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavbarComponent />}>
+          <Route index element={<AddExpenseComponent />} />
+          <Route path="group" element={<AddGroup />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
+
+    // <div>
+    //   <NavbarComponent />
+    //   {/* <AddExpenseComponent></AddExpenseComponent> */}
+    //   {/* <AddGroup></AddGroup> */}
+
+    // </div>
   );
 }
 
--- expense-app/src/components/NavbarComponent.jsx
+++ expense-app/src/components/NavbarComponent.jsx
@@ -1,20 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Outlet, Link, NavLink } from "react-router-dom";
 
 function NavbarComponent() {
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Expense App</Navbar.Brand>
+                    <Navbar.Brand as={NavLink} to="/">Expense App</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/group">Group</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
+
+            <Outlet />
         </>
     );
 }
