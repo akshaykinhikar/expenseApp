fc6b6baa7ffef62809ba3ed875a44d86a97a69b7
akshaykinhikar <kinhikar.akshay32@gmail.com>
2024-04-21 19:40:39 +0530

    UI-footer added

--- expense-app/src/components/footer.jsx
+++ expense-app/src/components/footer.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 const Footer = () => <footer className="footer font-small blue ">
-    <div className="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://mdbootstrap.com/"> kinhikar.akshay32@gmail.com</a>
+    {/*  */}
+    <div class="Footer-section">
+        <div class="triangle"></div>
+        <h3>Thank You</h3>
+        {/* <p>CompSciLauren@gmail.com</p> */}
+        <div className="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="https://mdbootstrap.com/"> kinhikar.akshay32@gmail.com</a>
+        </div>
+        <p>Linkedin, Twitter, GitHub</p>
     </div>
 </footer>
 
