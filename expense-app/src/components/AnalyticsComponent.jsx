9d4c54c7d413a25e14a960f4a41bf08d32a131bf
akshaykinhikar <kinhikar.akshay32@gmail.com>
2024-07-07 11:41:20 +0530

    intermediate commit

--- expense-app/src/components/AnalyticsComponent.jsx
+++ expense-app/src/components/AnalyticsComponent.jsx
@@ -24,11 +24,13 @@ const AnalyticsComponent = (props) => {
         ],
     });
 
+    const colorsArray = ['#EE4E4E', '#FFC700', '#FFF455', '#219C90', '#81A263', '#77E4C8', '#405D72', '#F4CE14', '#FF7F3E'];
+
 
     function random_rgba(opacity) {
         let o = Math.round, r = Math.random, s = 255;
 
-        let last = opacity ? opacity : r().toFixed(1);
+        let last = 1; //opacity ? opacity : r().toFixed(1.0);
 
         return 'rgba('
             + o(r() * s)
@@ -47,10 +49,10 @@ const AnalyticsComponent = (props) => {
         data.datasets[0].borderColor = [];
         data.datasets[0].data = [];
         props.expenseSummary && props.expenseSummary.memShares &&
-            props.expenseSummary.memShares.length > 0 && props.expenseSummary.memShares.map(share => {
+            props.expenseSummary.memShares.length > 0 && props.expenseSummary.memShares.map((share, i) => {
                 data.labels.push(share.member.label);
-                let bgColor = random_rgba(0.5);
-                let borderColor = random_rgba(1);
+                let bgColor = colorsArray[i] ? colorsArray[i] : random_rgba(1);
+                let borderColor = colorsArray[i] ? colorsArray[i] : random_rgba(1);
                 let expense = share.totalExpenseByMember ? share.totalExpenseByMember : 0;
 
                 data.datasets[0].backgroundColor = [...data.datasets[0].backgroundColor, bgColor];
