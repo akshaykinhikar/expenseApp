e2b8709057ebdf2a4d811ea159d64be253a0dccc
akshaykinhikar <knhikar.akshay32@gmail.com>
2024-09-08 12:30:54 +0530

    UI-IT-backend-frontend

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -11,8 +11,8 @@ const connectDB = async () => {
 
         console.log(`MongoDB Connected: ${conn.connection.host}`)
 
-    } catch (error) {
-        // console.error(`Error: ${error.message}`);
+    } catch (error: any) {
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 }
